bcd2ccab1788b1c5d7cd5f3f2e01c9b284424c2a
tranhuuhuy297 <tranhuuhuy297@gmail.com>
2023-09-29 11:09:55 +0700

    local func same package

--- function/function.go
+++ function/function.go
@@ -4,8 +4,11 @@ import "fmt"
 
 // go lang không có khởi tạo giá trị mặc định cho param trong go lang
 
+// go run function/*
+
 func greet(name string) {
 	fmt.Printf("%v\n", name)
+	test()
 }
 
 func return_list(test []string) []string {
