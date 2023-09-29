ba10df4947f8f5fc7a8e6bed5f62dec946d428fe
tranhuuhuy297 <tranhuuhuy297@gmail.com>
2023-09-29 10:17:01 +0700

    function

---
+++ function/function.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+// go lang không có khởi tạo giá trị mặc định cho param trong go lang
+
+func greet(name string) {
+	fmt.Printf("%v\n", name)
+}
+
+func return_list(test []string) []string {
+	return test
+}
+
+func main() {
+	greet("huy")
+	fmt.Println([]string{"trần", "hữu", "huy"})
+}
