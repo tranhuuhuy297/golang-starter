299cad098b6e4d0dc92557feadaee7ecd68931fb
tranhuuhuy297 <tranhuuhuy297@gmail.com>
2023-10-15 17:09:49 +0700

    waitgroup

--- wait-group/wait.go
+++ wait-group/wait.go
@@ -9,14 +9,14 @@ func main() {
 	wg := sync.WaitGroup{}
 	wg.Add(1)
 
-	go print(wg)
+	go print(&wg)
 
 	wg.Wait()
 
 	fmt.Println("done")
 }
 
-func print(wg sync.WaitGroup) {
+func print(wg *sync.WaitGroup) { // Phải truyền pointer vào để tránh việc truyền bản sao ->
 	fmt.Println("hello from goroutine")
 	wg.Done()
 }
